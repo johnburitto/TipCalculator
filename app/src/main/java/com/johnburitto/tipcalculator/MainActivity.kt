8892ff93e811ebcf1c0948f0ce80e53c43135691
johnburitto <79087305+johnburitto@users.noreply.github.com>
2023-02-18 17:23:56 +0200

    Working tip calculator

--- app/src/main/java/com/johnburitto/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/johnburitto/tipcalculator/MainActivity.kt
@@ -2,10 +2,44 @@ package com.johnburitto.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.johnburitto.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val serviceCost = binding.costOfService.text.toString().toDoubleOrNull()
+
+        if (serviceCost == null) {
+            binding.tipResult.text = ""
+            return
+        }
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        val tip = if (binding.roundTip.isChecked) ceil(serviceCost * tipPercentage)
+                    else serviceCost * tipPercentage
+
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip : Double) {
+
+        val formattedTip = NumberFormat.getInstance().format(tip)
+
+        binding.tipResult.text = getString(R.string.tip_result_text, formattedTip)
     }
 }
